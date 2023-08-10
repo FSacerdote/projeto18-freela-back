b267c5237d12dfd52fba00d12626026dcf7c5826
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-08-09 23:56:16 -0300

    feat: controllers

---
+++ src/controllers/gatos.controllers.js
@@ -0,0 +1,15 @@
+export async function getGatos(){
+
+}
+
+export async function getGatosById(){
+    
+}
+
+export async function getMyGatos(){
+    
+}
+
+export async function postGato(){
+    
+}
