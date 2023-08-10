04c49dbbd9d232a608d82e580f1c5370378149f5
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-08-10 01:17:21 -0300

    feat: gatos controllers parte 1

--- src/controllers/gatos.controllers.js
+++ src/controllers/gatos.controllers.js
@@ -1,15 +1,27 @@
-export async function getGatos(){
+export async function getGatos(req, res) {
+    try {
+        const gatos = await db.query(`SELECT * FROM gatos;`)
+        res.send(gatos.rows)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
+
+export async function getGatosById(req, res) {
+    const { id } = req.params
+    try {
+        const gato = await db.query(`SELECT * FROM gatos WHERE id=$1`, [id])
+        if(gato.rowCount === 0) return res.sendStatus(404)
+        res.send(gato)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
+
+export async function getMyGatos(req, res) {
 
 }
 
-export async function getGatosById(){
-    
-}
+export async function postGato(req, res) {
 
-export async function getMyGatos(){
-    
-}
-
-export async function postGato(){
-    
 }
