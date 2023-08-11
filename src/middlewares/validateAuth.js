63c5e67c88330bfe63cddbbd1ddc4b0cd05459e3
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-08-10 22:03:31 -0300

    feat: validateAuth middleware

---
+++ src/middlewares/validateAuth.js
@@ -0,0 +1,15 @@
+import { db } from "../database/database.connection.js"
+
+export default async function validateAuth(req, res, next) {
+    const { authorization } = req.headers
+    const token = authorization?.replace("Bearer ", "")
+    if (!token) return res.sendStatus(401)
+    try {
+        const sessao = await db.query("SELECT * FROM sessions WHERE token=$1;", [token])
+        if(sessao.rowCount === 0) return res.sendStatus(401)
+        res.locals.userId = sessao.rows[0].idusuario
+        next()
+    } catch (error) {
+        console.log(error.message)
+    }
+}
